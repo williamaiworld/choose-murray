61e3a1b966eb1ed8c4bdfa21f9a308fd091af914
William Klemm <williamaiworld@gmail.com>
2020-01-23 21:28:06 -0800

    initial completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const UserCard = ({user}) => {
+  console.log('>>>>>> user', user)
+  if(!user) return '';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.name}
+      <img src={user.imgURL} />
     </div>
+  )
+}
+
+let id = 300;
+const murrays = [
+  {
+    imgURL: `https://www.fillmurray.com/g/250/${id}`,
+    id,
+    name: 'Bill',
+  },
+  {
+    imgURL: `https://www.fillmurray.com/g/250/${++id}`,
+    id,
+    name: 'Billy',
+  },
+  {
+    imgURL: `https://www.fillmurray.com/g/250/${++id}`,
+    id,
+    name: 'Billy Bob',
+  },
+  {
+    imgURL: `https://www.fillmurray.com/g/250/${++id}`,
+    id,
+    name: 'What About Bob',
+  },
+]
+const getFirstId = users => users[0].id
+function App() {
+  const [currentDirection, setDirection] = useState(false)
+  const [murrayArray, setMurrayArray] = useState(murrays);
+  const [chosenId, setChosenId] = useState(murrays[0].id);
+  const changeDir = () => {
+    console.log('>>>>>> chosenId', chosenId);
+    setDirection(!currentDirection)
+    murrayArray.sort((murray1, murray2) => {
+      return currentDirection ? murray1.id - murray2.id : murray2.id - murray1.id
+    })
+    setMurrayArray([...murrayArray]);
+  }
+  const getMurrayById = id => murrayArray.filter(murray => murray.id === chosenId)[0];
+  return (
+    <main className="App">
+      <div>Chosen Murray</div>
+      <UserCard user={getMurrayById(chosenId)} />
+      <div>
+      
+        <h1>Choose Murray</h1>
+        <button onClick={changeDir}>Change Direction</button>
+        <div className="list-container">
+        {murrayArray.map(murray => (
+          <div className = "list-item" onClick={() => setChosenId(murray.id)}>
+            <UserCard user={murray} />
+          </div>
+        ))}
+        </div>
+      </div>
+
+    </main>
   );
 }
 
